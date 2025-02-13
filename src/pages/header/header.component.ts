16cf847d6eb1c6ccd4a54d5fd0c21417d1f6229b
BDhanush489 <bdhanush489@gmail.com>
2025-02-13 18:09:58 +0530

    2s

--- src/pages/header/header.component.ts
+++ src/pages/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  redirectToLogin() {
+    this.router.navigate(['/login']);
+  }
 
 }
